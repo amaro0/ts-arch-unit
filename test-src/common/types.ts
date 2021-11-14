6bf0d44b9b5d4de153065c157cba94f2c71604d8
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-11-14 19:00:57 +0100

    feat: add some test project src

---
+++ test-src/common/types.ts
@@ -0,0 +1,3 @@
+export type Context = { transaction: Record<string, unknown> };
+
+export type Id = string;
---
+++ test-src/domains/users/core/ports/ICreateUser.ts
@@ -0,0 +1,6 @@
+import { Context, Id } from '../../../../common/types';
+import { CreateUserCommand } from '../models/CreateUserCommand';
+
+export interface ICreateUser {
+  handle(command: CreateUserCommand, ctx?: Context): Promise<Id>
+}
